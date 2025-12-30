7585d1f36b419c026641d2e814319bd7b04099eb
Asutosh-1234 <pandaashutosh220@gmail.com>
2025-12-30 22:15:38 +0530

    Revert "bug"
    This reverts commit 4d9358f23017668b966d4230a25f235b082d5ec0.

--- hello.js
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-console.log("Hello everyone");
-here is a bugggg
-console.log("hello");
-=======
-console.log("Hello everyone");
->>>>>>> parent of abdc6d4 (bug)
--- hi.js
+++ hi.js
@@ -1,5 +1,8 @@
 console.log("hello");
+<<<<<<< HEAD
 
 bug
 
 console.log("hii");
+=======
+>>>>>>> parent of 4d9358f (bug)
